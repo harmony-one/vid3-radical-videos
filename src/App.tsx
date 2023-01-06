9a31ee3d907f375bf9d6adc2b769ab94138806f0
ahiipsa <ahiipsa@gmail.com>
2023-01-06 19:35:44 +0400

    fix eslit error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-import logo from './logo.svg';
 import './App.css';
 import VideoHome from './routes/video-home';
 import VideoReels from './routes/video-reels';
